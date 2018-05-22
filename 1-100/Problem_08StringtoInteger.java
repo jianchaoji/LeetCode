bba6adfbd5bee9c2a6f503f600b8e36977153028
jianchao ji <32049203+jianchaoji@users.noreply.github.com>
2018-05-22 17:05:41 -0400

    change

--- 1-100/Problem_08StringtoInteger.java
+++ 1-100/Problem_08StringtoInteger.java
@@ -1,6 +1,39 @@
 
 public class Problem_08StringtoInteger {
-public int myAtoi(String str) {
-        return 0;
+  public int myAtoi(String str) {
+	  
+	  int i=0;
+	  int flat=1;
+	  while(i<str.length()&&str.charAt(i)==' ')i++;
+	  
+	  if(i<str.length()) {
+		  if(str.charAt(i)=='-') {
+			  i++;
+			  flat=-1;
+		  }
+		  if(str.charAt(i)=='+') {
+			  i++;
+			  flat=1;
+		  }
+	  }
+	  
+	  
+	  long num=0;
+	  while(i<str.length()) {
+		  if(str.charAt(i)<='9'&&str.charAt(i)>='0') {
+			  num=num*10+str.charAt(i)-'0';
+			  
+			  if(num>Integer.MAX_VALUE+1)break;
+		  }
+		  
+		  
+		  else break;
+		  i++;
+	  }
+	  
+	  if(flat==1&&num>Integer.MAX_VALUE)return Integer.MAX_VALUE;
+	  if(flat==-1&&num>Integer.MAX_VALUE+1)return Integer.MIN_VALUE;
+	  
+        return (int) (flat*num);
     }
 }
