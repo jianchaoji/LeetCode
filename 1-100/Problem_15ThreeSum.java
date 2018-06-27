b22e815afe06190b3c01e5ddc7dd10519b179311
jianchao ji <32049203+jianchaoji@users.noreply.github.com>
2018-06-27 18:03:04 -0400

    15

--- 1-100/Problem_15ThreeSum.java
+++ 1-100/Problem_15ThreeSum.java
@@ -4,7 +4,18 @@ import java.util.*;
 public class Problem_15ThreeSum {
 public List<List<Integer>> threeSum(int[] nums) {
 	Arrays.sort(nums);
-	
+	 List<List<Integer>> res= new LinkedList<>();
+	 for(int i=0;i<nums.length-2;i++) {
+		 int lo=i+1;
+		 int hi=nums.length-1;
+		 int sum=0-nums[i];
+		 if(i==0||(i>0&&nums[i]!=nums[i-1])) {
+			 if(sum+nums[lo]+nums[hi]==0) {
+				 res.add(Arrays.asList(nums[i],nums[lo],nums[hi]));
+				 
+			 }
+		 }
+	 }
 	
 	
 	return null;
