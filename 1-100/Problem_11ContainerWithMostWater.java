989ad0a58ceef73a57bd2a19aec51bebd2019645
jianchao ji <32049203+jianchaoji@users.noreply.github.com>
2018-08-14 13:50:47 -0400

    finished

--- 1-100/Problem_11ContainerWithMostWater.java
+++ 1-100/Problem_11ContainerWithMostWater.java
@@ -1,6 +1,35 @@
 
 public class Problem_11ContainerWithMostWater {
-    public int maxArea(int[] height) {
-        return 0;
+    static public int maxArea(int[] height) {
+    	
+    	int i=0;
+    	int j=height.length-1;
+    	int max=0;
+    	while(i<j) {
+    		
+    		int heightI=height[i];
+    		int heightJ=height[j];
+    		int area=0;
+    		if(heightI>=heightJ) {
+    			area=(j-i)*heightI;
+    			i++;
+    		}
+    		else {
+    			area=(j-i)*heightJ;
+    			j--;
+    		}
+    		
+    		max=Math.max(area, max);
+    	}
+    	
+        return max;
+        
+        
+    }
+    
+    public static void main(String[] arcs) {
+    	int[] height={1,1,2};
+    	maxArea(height);
+    	
     }
 }
--- 1-100/Problem_16ThreeSumCloset.java
+++ 1-100/Problem_16ThreeSumCloset.java
@@ -6,26 +6,29 @@ public class Problem_16ThreeSumCloset {
 		 
 		 Arrays.sort(nums);
 		 int result=nums[0]+nums[1]+nums[nums.length-1]-target;
+		 int final_result=nums[0]+nums[1]+nums[nums.length-1];
 		 for(int i=0;i<nums.length-2;i++) {
 			 int lo=i+1;
 			 int hi=nums.length-1;
 			 while(lo<hi) {
 			 int sum=nums[i]+nums[lo]+nums[hi];
+			 
 			 int temp;
 			 temp=sum-target;
-			 if(result>0) {
+			 if(temp>0) {
 				 hi--;
 			     }else {
 				 lo++;
 			            }
 				 if(Math.abs(temp)<Math.abs(result)) {
 					 result=temp;
+					 final_result=sum;
 				 }
 			 }
 		 }
 		 
 		 
-	        return result;
+	        return final_result;
 	    }
 	
 	public static void main(String[] args) {
