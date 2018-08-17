f45eeab044e596212e1ff0e9238cc8662d8c9327
jianchao ji <32049203+jianchaoji@users.noreply.github.com>
2018-08-17 18:11:27 -0400

    finished

--- 1-100/Problem_19RemoveNthNodeFromEndofList.java
+++ 1-100/Problem_19RemoveNthNodeFromEndofList.java
@@ -1,25 +1,31 @@
 
 public class Problem_19RemoveNthNodeFromEndofList {
 	
-	public class ListNode {
+	/*public class ListNode {
 		   
 		int val;
 	    ListNode next;
 	    ListNode(int x) { val = x; }
-	}
+	}*/
 	 public ListNode removeNthFromEnd(ListNode head, int n) {
-		 
-		 ListNode temp=new ListNode(0);
-		 temp=head;
-		 for(int i=0;i<n;i++) {
-			 temp=temp.next;
-			 
-		 }
-		 
-		if(temp.next!=null)temp.next=temp.next.next;
+		ListNode start=new ListNode(0);
+		ListNode slow=start,fast=start;
+		start.next=head;
 		
-		 
-	        return head;
+		for(int i=0;i<n;i++) {
+			fast=fast.next;
+		}
+		
+		while(fast.next!=null) {
+			slow=slow.next;
+			fast=fast.next;
+		}
+		
+		slow.next=slow.next.next;
+		
+		
+		return start.next;
+		
 	    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
